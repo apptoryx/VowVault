5807141758802a3cd12bafa4a0deec9515ee5a69
ApptoryX <gardiniatakt@gmail.com>
2025-08-02 10:14:10 +0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCSz09qICZ9Sy73NWJle966eHfnzAOF0h4",
+  authDomain: "vow-vault-72522.firebaseapp.com",
+  projectId: "vow-vault-72522",
+  storageBucket: "vow-vault-72522.firebasestorage.app",
+  messagingSenderId: "937363997566",
+  appId: "1:937363997566:web:e1f92385ee44150a65692f",
+  measurementId: "G-DL4QE2PVK1"
+};
